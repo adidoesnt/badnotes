3d9f2b978a8f2e3d265632e26cde75696b105987
Banerjee Aditya <aditya.banerjee@activate.sg>
2023-11-09 14:02:14 +0800

    fix linting issues

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import { json, urlencoded } from "body-parser";
 import { initRoutes } from "./routes";
-import { findAll } from "./repository/read";
 
 const { SERVER_PORT } = process.env;
-const PORT = SERVER_PORT || 8080;
+const PORT = SERVER_PORT ?? 8080;
 
 const app = express();
 app.use(json());
